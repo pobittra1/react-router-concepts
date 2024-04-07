c8f40de3b2675e0acbf66756f7336f06c96c3152
pobi <pobittrachandraroy2001@gmail.com>
2024-04-07 19:40:52 +0600

    implement useNavigation or loading method

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,25 +1,27 @@
-import { Outlet , useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 
-
-
 const Home = () => {
-    const location = useLocation();
-    console.log(location)
-    const headingStyle = {
-        fontSize: "30px",
-        color:"red"
-        
-    }
+  const location = useLocation();
+  const navigation = useNavigation();
+  console.log(location);
+  const headingStyle = {
+    fontSize: "30px",
+    color: "red",
+  };
 
-    return (
-        <div> 
-            <h2 style={headingStyle}>this is home</h2>
-            <Header></Header>
-            <p>this is home</p>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div>
+      <h2 style={headingStyle}>this is home</h2>
+      <Header></Header>
+      <p>this is home</p>
+      {navigation.state === "loading" ? (
+        <p>loading......</p>
+      ) : (
+        <Outlet></Outlet>
+      )}
+    </div>
+  );
 };
 
 export default Home;
